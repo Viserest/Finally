5423234b1e7a1c6cc89a75409b199a414fcbb1e3
Viserest <50747852+Viserest@users.noreply.github.com>
2021-09-29 14:16:29 -0500

    Update lib.py

--- lib.py
+++ lib.py
@@ -1,14 +1,23 @@
+class Players:
+  def addPlayer(self, options):
+    self.players.append()
+  def editPlayer(self, index, options):
+    for k,v in options.items():
+      self.players[index][k] = v
+  def removePlayer(self, index):
+    self.players.pop(index)
+
 # Minecraft Class
 class Minecraft:
-  class Game(PlayerI):
-    def __init__(self, options):
-      for k,v in options.items():
-        self.__dict__[k] = v
-    def addPlayer(self, options): self.players.append(Minecraft.Player(options))
-    def 
-  class Player:
-    def __init__(self, options):
-      for k,v in options.items():
-        self.__dict__[k] = v
+  def __init__(self, options):
+    for k,v in options.items():
+      self.__dict__[k] = v
+  def addPlayer(self, options):
+    self.players.append(Minecraft.Player(options))
+  def editPlayer(self, index, options):
+    for k,v in options.items():
+      self.players[index][k] = v
+  def removePlayer(self, index):
+    self.players.pop(index)
 game = Minecraft({'name':'LastLife','players':[]})
 game.players.append({'name':'Viserest','hearts':20})
