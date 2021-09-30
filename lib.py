9432056ac207084428df657cd60540718ca9c869
Viserest <50747852+Viserest@users.noreply.github.com>
2021-09-30 13:52:24 -0500

    Update lib.py

--- lib.py
+++ lib.py
@@ -1,12 +1,31 @@
-class Entities:
-  def __init__(self, options):
-    self.players = []
+class Context:
+  storage = self.__dict__[self.__dict__['storage']]
+  def addEntity(self, options):
+    self[storage].append({k:v for k,v in options.items()})
+  def editEntity(self, index, options):
     for k,v in options.items():
-      self.__dict__[k] = v
+      if (v == None) self.entities[index].pop(k)
+      self.entities[index][k] = v
+  def removeEntity(self, index):
+    self.entities.pop(index)
 
-class Minecraft:
-  def __init__(self, options):
-    self.players = []
+class Minecraft(Context):
+  def game(self, options):
+    self.entities = []
     for k,v in options.items():
       self.__dict__[k] = v
-class 
+class Calendar:
+  events = []
+  def addEvent(self, options):
+    Calendar.events.append({k:v for k,v in options.items()})
+  def editEvent(self, index, options):
+    for k,v in options.items():
+      Calendar.events[index][k] = v
+  def removeEvent(self, index):
+    Calendar.events.pop(index)
+# test
+game = Minecraft.game({'name':'LastLife'})
+game.addEntity({'name':'MumboJumbo', 'lives':3, 'boogieMan':False})
+game.editEntity({'boogieMan':None, 'boogeyMan':False})
+
+Calendar.addEvent()
