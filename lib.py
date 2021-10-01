8a961a7e1d586504c7a21feb6e32a22ec2e6d94a
Viserest <50747852+Viserest@users.noreply.github.com>
2021-09-30 23:34:37 -0500

    Update lib.py

--- lib.py
+++ lib.py
@@ -1,31 +1,45 @@
-class Context:
-  storage = self.__dict__[self.__dict__['storage']]
-  def addEntity(self, options):
-    self[storage].append({k:v for k,v in options.items()})
-  def editEntity(self, index, options):
-    for k,v in options.items():
-      if (v == None) self.entities[index].pop(k)
-      self.entities[index][k] = v
-  def removeEntity(self, index):
-    self.entities.pop(index)
+import json
 
-class Minecraft(Context):
-  def game(self, options):
-    self.entities = []
+class Data:
+  def save(self, file):
+    with open(file, 'w') as f:
+      json.dump(self.__dict__, f, indent=4)
+  def load(self, file):
+    with open(file) as f:
+      self.__dict__ = f
+
+class Minecraft(Data):
+  def __init__(self, options):
+    self.players = []
     for k,v in options.items():
       self.__dict__[k] = v
-class Calendar:
-  events = []
-  def addEvent(self, options):
-    Calendar.events.append({k:v for k,v in options.items()})
-  def editEvent(self, index, options):
+  def addPlayer(self, options):
+    self.players.append({
+      k:v for k,v in options.items()
+    })
+  def getPlayer(self, index):
+    print(self.player[index])
+  def editPlayer(self, index, options):
     for k,v in options.items():
-      Calendar.events[index][k] = v
-  def removeEvent(self, index):
+      if v == None: self.players[index].pop(k)
+      else: self.players[index][k] = v
+  def removePlayer(self, index):
+    print(self.players[index])
+    if (input('Are you sure you want to delete this? (y,n)> ') == 'n'): return
+    self.players.pop(index)
+class Calendar(Data):
+  events = []
+  def addEvent(options):
+    Calendar.events.append({
+      k:v for k,v in options.items()
+    })
+  def getEvent(time):
+    print(e for e in Calendar.events if time in e['time'])
+  def editEvent(index, options):
+    for k,v in options.items():
+      if v == None: Calendar.events[index].pop(k)
+      else: Calendar.events[index][k] = v
+  def removeEvent(index):
+    print(Calendar.events[index])
+    if (input('Are you sure you want to delete this? (y,n)> ') == 'n'): return
     Calendar.events.pop(index)
-# test
-game = Minecraft.game({'name':'LastLife'})
-game.addEntity({'name':'MumboJumbo', 'lives':3, 'boogieMan':False})
-game.editEntity({'boogieMan':None, 'boogeyMan':False})
-
-Calendar.addEvent()
